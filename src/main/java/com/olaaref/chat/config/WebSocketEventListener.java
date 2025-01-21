08775fe3098cf3b9def85ce885b9ded367e9b178
OlaAref <olaaref1@gmail.com>
2025-01-21 21:55:06 +0200

    :sparkles: Finish the disconnect event listener

--- src/main/java/com/olaaref/chat/config/WebSocketEventListener.java
+++ src/main/java/com/olaaref/chat/config/WebSocketEventListener.java
@@ -1,8 +1,12 @@
 package com.olaaref.chat.config;
 
+import com.olaaref.chat.dto.ChatMessage;
+import com.olaaref.chat.enums.MessageType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -10,10 +14,29 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @RequiredArgsConstructor
 @Slf4j
 public class WebSocketEventListener {
+    private final SimpMessageSendingOperations messagingTemplate;
 
+    /**
+     * Handles WebSocket disconnection events.
+     * This method listens for `SessionDisconnectEvent` and processes the event when a user disconnects.
+     * It retrieves the username from the WebSocket session attributes and logs the disconnection.
+     * A `ChatMessage` of type `LEAVE` is then created and broadcasted to the "/topic/public" topic.
+     *
+     * @param event the WebSocket session disconnect event
+     */
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
-        log.info("Received a new web socket connection");
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if(username != null) {
+            log.info("User Disconnected: {}", username);
+            var chatMessage = ChatMessage
+                    .builder()
+                    .type(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+            messagingTemplate.convertAndSend("/topic/public", chatMessage);
+        }
     }
 
 }
